64df56fb63f80c3722fb812dfab6b54c10acb2bd
fanhl <dyguests@qq.com>
2017-05-31 10:47:44 +0800

    splash

---
+++ app/src/main/java/com/fanhl/ppt/ui/square/RoomActivity.java
@@ -0,0 +1,34 @@
+package com.fanhl.ppt.ui.square;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.fanhl.ppt.R;
+
+/**
+ * 房间，一个团在一个房间
+ */
+public class RoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
